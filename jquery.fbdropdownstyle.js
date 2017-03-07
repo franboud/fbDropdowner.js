7b6fb4b65e6a7475284ab0d81bdb3206bee3691f
franboud <francis@franboud.com>
2017-03-07 11:39:56 -0400

    Initial commit.
    Version 1.0

---
+++ jquery.fbdropdownstyle.js
@@ -0,0 +1,119 @@
+/**
+ * jquery.fbdropdownstyle.js
+ * Version 1.0
+ * March 2017
+ *
+ * Dropdown list styling (<select>).
+ * Because styling dropdown lists for browsers is impossible!
+ * This plugin transforms the <select> tag to <dl>, <dt> and <dd> tags, which are more stylable.
+ * Don't forgot to include the fbdropdownstyle.css file.
+ *
+ * Required:
+ *    - jQuery (tested on jQuery v3.1.1)
+ *    - ScrollTo (tested on 2.1.2)
+ *    - ScrollMagic (tested on v2.0.5)
+ *
+ * Pour activer :
+ *    $(".js-dropdown").fbdropdownstyle();
+ *
+ * Options configurables :
+ *    - section_class --> .js-quicknav-section --> Sections added in the quick nav.
+ *    - section_title --> data-quicknav-title --> Data attribute for the title of the section, displayed in the quick nav.
+ *    - scroll_offset --> -100 (px) --> Offset of the scroll when we click on a quick nav link.
+ *    - scroll_duration --> 500 (ms) --> Duration of the scroll.
+ */
+
+(function ($) {
+
+    $.fn.fbdropdownstyle = function (options) {
+
+        // Default options.
+        var settings = $.extend({
+            section_class: ".js-quicknav-section",
+            section_class_hide: ".js-quicknav-hide",
+            section_title: "data-quicknav-title",
+            scroll_offset: -100,
+            scroll_duration: 500
+        }, options);
+
+
+        // Global vars
+        var
+            $dropdown,
+            $styleable_dropdown;
+
+
+        return this.each(function () {
+            $dropdown = $(this);
+
+            create_stylable_dropdown();
+            create_events();
+        });
+
+
+        // TRANSFORMS the <select> to more stylable tags
+        // Those tags are <dl>, <dt> and <dd>
+        function create_stylable_dropdown() {
+            var
+                selected_option = $dropdown.find("option[selected]"),
+                $options = $("option", $dropdown);
+
+            $styleable_dropdown = $('<dl class="fbdropdownstyle"></dl>');
+
+            // Si aucun selected option par defaut, prendre le premier.
+            if (selected_option.length === 0) {
+                selected_option = $dropdown.find("option:first");
+            }
+
+            // DROPDOWN STYLABLE juste apres le dropdown std.
+            $dropdown.after($styleable_dropdown);
+
+            // Option par defaut
+            $styleable_dropdown.append('<dt><a href="#">' + selected_option.text() +
+                '<span class="value">' + selected_option.val() +
+                '</span></a></dt>');
+
+            // Autres options.
+            $styleable_dropdown.append('<dd><ul></ul></dd>');
+            $options.each(function () {
+                $("dd ul", $styleable_dropdown).append('<li><a href="#">' +
+                    $(this).text() + '<span class="value">' +
+                    $(this).val() + '</span></a></li>');
+            });
+
+        }
+
+
+        function create_events() {
+
+            // CLICK on the shown item opens the dropdown.
+            $("dt a", $styleable_dropdown).on("click", function (evt) {
+                evt.preventDefault();
+                $("dd ul", $styleable_dropdown).toggle();
+            });
+
+            // CLICK somewhere else in the document closes the dopdown.
+            $(document).on('click', function (evt) {
+                var $clicked = $(evt.target);
+                if (!$clicked.parents().hasClass("fbdropdownstyle"))
+                    $("dd ul", $styleable_dropdown).hide();
+            });
+
+            // CLICK on the selected item in the dropdown.
+            $("dd a", $styleable_dropdown).on("click", function (evt) {
+                evt.preventDefault();
+
+                // Select this item and closes the dropdown.
+                var text = $(this).html();
+                $("dt a", $styleable_dropdown).html(text);
+                $("dd ul", $styleable_dropdown).hide();
+
+                // Put the selected item in the original dropdown.
+                // And trigger a "change" event.
+                $dropdown.val($(this).find("span.value").html()).trigger("change");
+            });
+        }
+    };
+
+}(jQuery));
+
